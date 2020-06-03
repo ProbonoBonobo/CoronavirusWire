6b5532aec7ca38f1936db753e960768298934727
Kevin Zeidler <kzeidler@gmail.com>
2020-06-02 20:26:25 -0700

    updating extract_entities.py

--- src/coronaviruswire/extract_entities.py
+++ src/coronaviruswire/extract_entities.py
@@ -9,7 +9,7 @@ crawldb = db["moderationtable"]
 if __name__ == "__main__":
     updates = []
     print(f"Extracting entities..")
-    for row in [row for row in crawldb.find(ner=None, _limit=LIMIT_ARTICLES)]:
+    for row in [row for row in crawldb.find(ner=None, mod_status='approved', _limit=LIMIT_ARTICLES)]:
         print(f"Updating {row}")
         content = format_text(
             "\n".join(
@@ -18,22 +18,22 @@ if __name__ == "__main__":
                     for attr in [
                         row["title"],
                         row["summary"],
-                        row["keywords"],
                         row["content"],
                     ]
                     if attr
                 ]
             )
         )
-        ents = [ent.strip().title() for ent in extract_entities_with_allennlp(content)]
+        ents = [[ent.strip().title(), ent.strip()][int(bool('\'' in ent))] for ent in extract_entities_with_allennlp(content)]
         unique_ents = set(ents)
         counts = {}
         for ent in ents:
-            counts[ent] = content.count(ent)
-        updates.append(
-            {"article_id": row["article_id"], "ner": counts, "has_ner": True}
-        )
-        if updates and not len(updates) % 1000:
-            crawldb.update_many(updates, ["article_id"])
-            updates = []
+            counts[ent] = content.lower().count(ent)
+        # updates.append(
+        #     {"article_id": row["article_id"], "ner": counts, "has_ner": True}
+        # )
+        crawldb.update(  {"article_id": row["article_id"], "ner": counts, "has_ner": True}, ['article_id'])
+        # if updates and not len(updates) % 1000:
+        #     crawldb.update_many(updates, ["article_id"])
+        #     updates = []
     crawldb.update_many(updates, ["article_id"])
